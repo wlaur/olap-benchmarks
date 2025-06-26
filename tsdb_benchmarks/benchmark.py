471d0567fe8277a52b377f451ecc57265fdc8b72
William Laurén <lauren.william.a@gmail.com>
2025-06-26 18:28:05 +0300

    fill in boilerplate

--- tsdb_benchmarks/benchmark.py
+++ tsdb_benchmarks/benchmark.py
@@ -3,7 +3,7 @@ from typing import Literal, get_args
 from pydantic import BaseModel
 
 from .database import Database
-from .settings import DatabaseName
+from .settings import SETTINGS, DatabaseName
 
 BenchmarkStep = Literal["insert", "query", "append", "upsert"]
 
@@ -13,11 +13,11 @@ STEPS: list[BenchmarkStep] = list(get_args(BenchmarkStep))
 class QueryResult(BaseModel):
     iterations: int = 1
     elapsed_sec: float
-    peak_memory_mb: int
+    peak_memory_mb: float
 
 
 class InsertResult(QueryResult):
-    disk_usage_mb: int
+    disk_usage_mb: float
 
 
 class CombinedResult(BaseModel):
@@ -35,20 +35,72 @@ class BenchmarkResults(BaseModel):
     upsert: CombinedResult | None = None
 
 
+def average(values: list[float]) -> float:
+    assert values
+
+    if len(values) == 1:
+        return values[0]
+
+    # remove the highest value (assume this is a cold start)
+    values.sort()
+    values.pop()
+
+    return sum(values) / len(values)
+
+
 def benchmark_insert(db: Database) -> InsertResult:
-    return InsertResult()
+    elapsed: list[float] = []
+    memory: list[float] = []
+    disk: list[float] = []
+
+    for _ in range(SETTINGS.insert_iterations):
+        elapsed.append(1)
+        memory.append(1)
+        disk.append(1)
+
+    return InsertResult(
+        iterations=SETTINGS.insert_iterations,
+        elapsed_sec=average(elapsed),
+        peak_memory_mb=average(memory),
+        disk_usage_mb=average(disk),
+    )
 
 
 def benchmark_query(db: Database) -> QueryResult:
-    return QueryResult()
+    elapsed: list[float] = []
+    memory: list[float] = []
+
+    for _ in range(SETTINGS.query_iterations):
+        elapsed.append(1)
+        memory.append(1)
+
+    return QueryResult(
+        iterations=SETTINGS.query_iterations,
+        elapsed_sec=average(elapsed),
+        peak_memory_mb=average(memory),
+    )
 
 
 def benchmark_append(db: Database) -> CombinedResult:
-    return CombinedResult()
+    insert_result = benchmark_insert(db)
+    query_result = benchmark_query(db)
+
+    return CombinedResult(
+        iterations=SETTINGS.append_iterations,
+        insert=insert_result,
+        query=query_result,
+    )
 
 
 def benchmark_upsert(db: Database) -> CombinedResult:
-    return CombinedResult()
+    insert_result = benchmark_insert(db)
+    query_result = benchmark_query(db)
+
+    return CombinedResult(
+        iterations=SETTINGS.upsert_iterations,
+        insert=insert_result,
+        query=query_result,
+    )
 
 
 def benchmark(db: Database, steps: BenchmarkStep | list[BenchmarkStep] | Literal["all"]) -> BenchmarkResults:
--- tsdb_benchmarks/settings.py
+++ tsdb_benchmarks/settings.py
@@ -12,6 +12,12 @@ class Settings(BaseSettings):
     database_directory: DirectoryPath
     results_directory: DirectoryPath
 
+    insert_iterations: int = 1
+    query_iterations: int = 10
+
+    append_iterations: int = 1
+    upsert_iterations: int = 1
+
     model_config = SettingsConfigDict(env_prefix="TSDB_BENCHMARKS_", extra="ignore")
 
 
