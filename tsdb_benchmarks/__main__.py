0bfa0604813559c0d1eee51d4e337488aae86294
William Laurén <lauren.william.a@gmail.com>
2025-06-27 18:58:22 +0300

    implement fetch for monetdb

--- tsdb_benchmarks/__main__.py
+++ tsdb_benchmarks/__main__.py
@@ -1,3 +1,6 @@
+import os
+from typing import Literal
+
 from fire import Fire  # type: ignore[import-untyped]
 
 from .settings import DatabaseName
@@ -7,9 +10,27 @@ def benchmark(name: DatabaseName) -> None:
     pass
 
 
+def run(name: DatabaseName, command: Literal["start", "stop"]) -> None:
+    match name:
+        case "monetdb":
+            from .monetdb import MonetDB
+
+            os.system(getattr(MonetDB(), command))
+
+        case _:
+            raise ValueError(f"Unknown database name: '{name}'")
+
+
+def query(name: DatabaseName, query: str) -> None:
+    match name:
+        case "monetdb":
+            from .monetdb import MonetDB
+
+            print(MonetDB().fetch(query))
+
+        case _:
+            raise ValueError(f"Unknown database name: '{name}'")
+
+
 if __name__ == "__main__":
-    Fire(
-        {
-            "benchmark": benchmark,
-        }
-    )
+    Fire({"benchmark": benchmark, "run": run, "query": query})
--- tsdb_benchmarks/monetdb/insert.py
+++ tsdb_benchmarks/monetdb/insert.py
@@ -1,11 +1,12 @@
 import polars as pl
+from sqlalchemy import Connection
 
 from ..settings import TableName
 
 
-def insert(df: pl.DataFrame, table: TableName) -> None:
+def insert(df: pl.DataFrame, table: TableName, connection: Connection) -> None:
     raise NotImplementedError
 
 
-def upsert(df: pl.DataFrame, table: TableName) -> None:
+def upsert(df: pl.DataFrame, table: TableName, connection: Connection) -> None:
     raise NotImplementedError
