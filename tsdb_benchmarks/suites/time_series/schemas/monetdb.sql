a2012181b4f828156d281a85d2a7a0ea40089cb4
William Laurén <lauren.william.a@gmail.com>
2025-07-19 18:12:58 +0300

    work on time series data

--- tsdb_benchmarks/suites/time_series/schemas/monetdb.sql
+++ tsdb_benchmarks/suites/time_series/schemas/monetdb.sql
@@ -1,25 +1,29 @@
 create table if not exists process_small_eav (
-    time timestamp primary key,
-    id smallint primary key,
-    value float32
+    time timestamp,
+    id smallint,
+    value real,
+    primary key (id, time)
 );
 
 create table if not exists process_medium_eav (
-    time timestamp primary key,
-    id smallint primary key,
-    value float32
+    time timestamp,
+    id smallint,
+    value real,
+    primary key (id, time)
 );
 
 create table if not exists process_large_eav (
-    time timestamp primary key,
-    id smallint primary key,
-    value float32
+    time timestamp,
+    id smallint,
+    value real,
+    primary key (id, time)
 );
 
 create table if not exists process_huge_eav (
-    time timestamp primary key,
-    id smallint primary key,
-    value float32
+    time timestamp,
+    id smallint,
+    value real,
+    primary key (id, time)
 );
 
 -- process_..._wide is created dynamically when inserted
