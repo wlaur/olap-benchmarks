90f00f3e0ac8bea481605b00e9e9c41079c32964
William Laurén <lauren.william.a@gmail.com>
2025-08-01 18:11:36 +0300

    rename queries

---
+++ tsdb_benchmarks/suites/time_series/queries/clickhouse/022_multiple_ids_raw_large_eav.sql
@@ -0,0 +1,46 @@
+select
+    time,
+    avg(
+        case
+            when id = 371 then value
+        end
+    ) as value_1,
+    cast(
+        avg(
+            cast(
+                case
+                    when id = 364 then value
+                end as Nullable(int)
+            )
+        ) as bool
+    ) as value_2,
+    avg(
+        case
+            when id = 407 then value
+        end
+    ) as value_3,
+    avg(
+        case
+            when id = 861 then value
+        end
+    ) as value_4,
+    avg(
+        case
+            when id = 984 then value
+        end
+    ) as value_5,
+    avg(
+        case
+            when id = 830 then value
+        end
+    ) as value_6
+from
+    data_large_eav
+where
+    id in (371, 364, 407, 861, 984, 830)
+group by
+    time
+order by
+    time
+limit
+    10000;
