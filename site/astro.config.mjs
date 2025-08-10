316687461ba7ac1f8c80af855addcd4fef7454ad
William Laurén <lauren.william.a@gmail.com>
2025-08-10 13:12:17 +0300

    fix username

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://willaur.github.io/olap-benchmarks/', // exact URL with trailing slash
+  site: 'https://wlaur.github.io/olap-benchmarks/', // exact URL with trailing slash
   base: '/olap-benchmarks/',                         // leading & trailing slash
 });
