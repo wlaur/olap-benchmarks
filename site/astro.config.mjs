491725aa031e08efbbe5dd2e815b3d4323fb6053
William Laurén <lauren.william.a@gmail.com>
2025-08-10 13:11:40 +0300

    add astro

---
+++ site/astro.config.mjs
@@ -0,0 +1,6 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://willaur.github.io/olap-benchmarks/', // exact URL with trailing slash
+  base: '/olap-benchmarks/',                         // leading & trailing slash
+});
