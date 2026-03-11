59196cda6b4a8d79dd1d97d15c49ace2effd67dd
William Laurén <lauren.william.a@gmail.com>
2026-03-11 19:49:27 +0200

    run

--- olap_benchmarks/suites/time_series/schemas/timescaledb/wide.sql
+++ olap_benchmarks/suites/time_series/schemas/timescaledb/wide.sql
@@ -14,19 +14,11 @@ SET
     );
 
 
--- TODO: data_large_wide cannot be compressed (too wide, 18k elements per row vs 8k max)
--- this is not the same limit as the 1_600 column limit for postgres, timescaledb is even more limited
--- SELECT
---     create_hypertable(
---         'data_large_wide',
---         'time',
---         chunk_time_interval => INTERVAL '7 days'
---     );
-
--- ALTER TABLE
---     data_large_wide
--- SET
---     (
---         timescaledb.enable_columnstore = true,
---         timescaledb.orderby = 'time'
---     );
+-- data_large_wide is too wide for Timescale columnstore/compression
+-- (18k elements per row vs 8k max), but plain hypertable chunking may still help.
+SELECT
+    create_hypertable(
+        'data_large_wide',
+        'time',
+        chunk_time_interval => INTERVAL '7 days'
+    );
