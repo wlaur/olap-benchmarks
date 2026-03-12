bc73f7908c2634fc094352ac1e283a80f9706a81
William Laurén <lauren.william.a@gmail.com>
2026-03-12 18:57:18 +0200

    dont use count distinct

--- olap_benchmarks/suites/time_series/queries/wide_16_count_distinct_days.sql
+++ olap_benchmarks/suites/time_series/queries/wide_16_count_distinct_days.sql
@@ -1,4 +1,6 @@
 select
-    count(distinct date_trunc('day', time)) as distinct_days
-from
-    data_wide
+    count(*) as distinct_days
+from (
+    select distinct date_trunc('day', time)
+    from data_wide
+) t
