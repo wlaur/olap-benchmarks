5c175870f606b52ca1436813ad9e44dd0dc5cf9b
William Laurén <lauren.william.a@gmail.com>
2026-03-12 11:47:42 +0200

    convert to hypertable after insert

--- olap_benchmarks/suites/time_series/schemas/timescaledb/wide.sql
+++ olap_benchmarks/suites/time_series/schemas/timescaledb/wide.sql
@@ -12,14 +12,3 @@ SET
         timescaledb.enable_columnstore = true,
         timescaledb.orderby = 'time'
     );
-
-
--- data_large_wide is too wide for Timescale columnstore/compression
--- (18k elements per row vs 8k max), but plain hypertable chunking still helps with queries
--- however, this makes the insert way slower (timescaledb-parallel-copy is not able to parallelize, uses 2 cores instead of 12)
-SELECT
-    create_hypertable(
-        'data_large_wide',
-        'time',
-        chunk_time_interval => INTERVAL '7 days'
-    );
