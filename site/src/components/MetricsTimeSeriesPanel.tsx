61665bce91cafa75fefec6f19c3119b3e537eb5d
William Laurén <lauren.william.a@gmail.com>
2026-03-13 11:55:48 +0200

    ui fixes

--- site/src/components/MetricsTimeSeriesPanel.tsx
+++ site/src/components/MetricsTimeSeriesPanel.tsx
@@ -151,7 +151,11 @@ export function MetricsTimeSeriesPanel({
                   </div>
 
                   <div className="h-40">
-                    <ResponsiveContainer width="100%" height="100%">
+                    <ResponsiveContainer
+                      width="100%"
+                      height="100%"
+                      initialDimension={{ width: 640, height: 160 }}
+                    >
                       <LineChart
                         data={chartData[selectedOperation][metric.key]}
                         syncId={`time-series-metrics-${selectedOperation}`}
