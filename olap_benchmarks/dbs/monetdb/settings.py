cf00662bda5f2f0cea944111cae9e24313458039
William Laurén <lauren.william.a@gmail.com>
2026-07-26 21:51:40 +0300

    Add MonetDB ADBC benchmark adapter

--- olap_benchmarks/dbs/monetdb/settings.py
+++ olap_benchmarks/dbs/monetdb/settings.py
@@ -5,7 +5,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     # binary fetch has a slight overhead (needs to infer the schema via PREPARE ... and process temporary files)
-    default_fetch_method: Literal["binary", "pymonetdb"] = "pymonetdb"
+    default_fetch_method: Literal["adbc", "binary", "pymonetdb"] = "adbc"
+    write_method: Literal["adbc", "staged"] = "adbc"
 
     # set to False to use copy ... on server (for binary export and csv or binary import)
     # this is faster if the client and server are on the same host
